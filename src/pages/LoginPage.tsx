a0996c84c93fc8933144c9cda826e138b73a8d3a
byeongdoohan <quden04@gmail.com>
2026-05-20 19:43:14 +0900

    오류 수정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import {Link, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {login} from "@/services/authApi";
 import {useAuthStore} from "@/store/authStore";
 import {Alert} from "@/components/ui/Alert";
